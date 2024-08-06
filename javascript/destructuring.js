1d3fbccb79fd5062778882e2d8fa86c47e28688f
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-08-06 21:36:13 +0530

    local sotrage

---
+++ javascript/destructuring.js
@@ -0,0 +1,7 @@
+// Destructuring in JavaScript is a convenient way to extract values from arrays or properties from objects into distinct variables. It allows you to unpack the values or properties into separate variables with a clean and readable syntax.
+
+const numbers = [1,2,3];
+const [a,b,c] = numbers;
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ javascript/higherorderfn.js
@@ -0,0 +1,13 @@
+//A function which takes another function as argument or returns a function 
+
+
+function x() {
+  console.log("Hello There")
+}
+
+
+function y(x) {
+  x();
+}
+
+y();
